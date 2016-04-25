5f1fe6a199df4ca21e4e1fb3e3438f5fb0dbc7c8
Marc Aupont <marc@digimarktech.com>
2016-04-25 14:42:42 -0400

    Initial Commit

---
+++ Pretty Simply Music App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pretty Simply Music App
+//
+//  Created by Marc Aupont on 4/25/16.
+//  Copyright © 2016 Digimark Technical Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
